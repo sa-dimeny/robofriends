a2006efa01cb01d9904644c7406f27f36a52e4c4
dimsandor <sandor.dim@gmail.com>
2019-02-24 19:22:12 +0200

    Updates

---
+++ precache-manifest.a36201c0f554fb5d90e2a117de0713ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "362dc4cfd9feeb1c0c15",
+    "url": "/robofriends/static/js/main.362dc4cf.chunk.js"
+  },
+  {
+    "revision": "330f755869b4fd88b582",
+    "url": "/robofriends/static/js/1.330f7558.chunk.js"
+  },
+  {
+    "revision": "362dc4cfd9feeb1c0c15",
+    "url": "/robofriends/static/css/main.d45a0235.chunk.css"
+  },
+  {
+    "revision": "330f755869b4fd88b582",
+    "url": "/robofriends/static/css/1.a6928e3b.chunk.css"
+  },
+  {
+    "revision": "3536281b3fac2fff054b4f9267d1a49f",
+    "url": "/robofriends/index.html"
+  }
+];
